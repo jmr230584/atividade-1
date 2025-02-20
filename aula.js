aae67c038de7235111c65d5c3d31024f062bcdda
José Mateus <mateusjoseph@gmail.com>
2025-02-20 11:20:39 -0300

    implementação classe Pessoa

---
+++ aula.js
@@ -0,0 +1,7 @@
+console.clear();
+//console.log(`Hello World!!`); 
+var nome = "";
+var idade = 18;
+var posicao = true;
+console.log("Ol\u00E1!! me chamo ".concat(nome, " Mateus e tenho ").concat(idade, " isso \u00E9 ").concat(posicao, " "));
+//console.log ("Olá!! me chamo " + nome + " Mateus"); 
---
+++ aula.ts
@@ -0,0 +1,15 @@
+console.clear();
+
+const pi: number = 3.14;
+
+var teste: string = `Teste 1 ok`;
+let testeDois: string = `Teste 2 ok`;
+
+
+if (true){
+    console.log(teste);
+    console.log(testeDois);
+
+    let testeTres: string = `Teste 3 ok`;
+    console.log(testeTres);
+}
